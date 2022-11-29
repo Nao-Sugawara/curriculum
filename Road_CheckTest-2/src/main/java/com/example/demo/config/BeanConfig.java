6f9ccd7a011ac70ab76df842c8ca16cffcd9e47e
naosugahara <105054689+naosugahara@users.noreply.github.com>
2022-11-29 20:22:15 +0900

    11/29 cheakTest

---
+++ Road_CheckTest-2/src/main/java/com/example/demo/config/BeanConfig.java
@@ -0,0 +1,18 @@
+//package com.example.demo.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import com.example.demo.dao.UserInfoMapper;
+//
+//@Configuration
+//public class BeanConfig {
+//	
+//@Bean
+//UserInfoMapper userInfoMapper(){
+//	UserInfoMapper userInfoMapper = new UserInfoMapper();
+//	return userInfoMapper;
+//	}
+//
+//
+//}
